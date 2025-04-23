7e54e8d96f9a7e39fb4cbb5bbeedde5e4fd37a99
mylegs-shake <cantstopshakinglegs@gmail.com>
2025-04-23 08:39:24 +0530

    Add files via upload

---
+++ Assignment1_Task1.py
@@ -0,0 +1,9 @@
+#Task 1
+
+a=int(input("Enter the 1st number: "))
+b=int(input("Enter the 2nd number: "))
+
+print("\nAddition: ", a+b)
+print("Substraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
---
+++ Assignment1_Task2.py
@@ -0,0 +1,11 @@
+#Assignment 1 Task 2
+'''Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+a=input("Enter your first name: ")
+b=input("Enter your last name: ")
+
+concatenated_name = a + " " + b + "!"
+print("Hello, ", concatenated_name," Welcome to the Pyton Program.")
