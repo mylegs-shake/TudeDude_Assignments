eca3d5de68ae6bd991e2bbbe970de3de5bef84e1
mylegs-shake <cantstopshakinglegs@gmail.com>
2025-04-23 08:40:26 +0530

    Assignment1_Task1.py

--- Assignment1_Task1.py
+++ Assignment1_Task1.py
@@ -1,5 +1,14 @@
 #Task 1
-
+'''
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+'''
 a=int(input("Enter the 1st number: "))
 b=int(input("Enter the 2nd number: "))
 
